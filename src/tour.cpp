168712852eb12b166607919636fd5a8bfd84661f
HenryI259 <hison259@gmail.com>
2025-12-07 17:22:17 -0600

    trial 8.0

--- src/tour.cpp
+++ src/tour.cpp
@@ -185,7 +185,7 @@ public:
         graph->add_edge(0,1);
         graph->add_edge(1,2);
         graph->add_edge(2,4);
-        graph->add_edge(3,4);
+        //graph->add_edge(3,4);
         graph->add_edge(1,3);
         // graph->add_edge(2,4);
         // graph->add_edge(3,4);
